735de452c1082874d02f75ba3d4e66004e3661e9
Darvidva <mbredavid8@gmail.com>
2026-06-18 05:11:37 +0100

    refactor: remove custom webpack externals config and update build command in netlify.toml

--- next.config.js
+++ next.config.js
@@ -2,12 +2,6 @@
 const nextConfig = {
   images: { unoptimized: true },
   serverExternalPackages: ['@prisma/client', 'prisma', 'pg'],
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals.push('@prisma/client', 'pg')
-    }
-    return config
-  }
 }
 
 module.exports = nextConfig;
